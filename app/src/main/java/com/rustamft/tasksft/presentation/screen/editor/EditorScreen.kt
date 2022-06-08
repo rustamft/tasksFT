d241b2815ba7f324aa59e0b002361ceeb33808a0
rustamft <rustam.ft@pm.me>
2022-06-08 23:13:03 +0500

    Draft 2022-06-08.

--- app/src/main/java/com/rustamft/tasksft/presentation/screen/editor/EditorScreen.kt
+++ app/src/main/java/com/rustamft/tasksft/presentation/screen/editor/EditorScreen.kt
@@ -1,21 +1,25 @@
 package com.rustamft.tasksft.presentation.screen.editor
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
 import androidx.compose.material.ScaffoldState
+import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -23,8 +27,8 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.rustamft.tasksft.R
-import com.rustamft.tasksft.domain.model.Task
 import com.rustamft.tasksft.domain.util.ROUTE_EDITOR
+import com.rustamft.tasksft.presentation.theme.DIMEN_SMALL
 
 @Destination(
     route = ROUTE_EDITOR,
@@ -34,14 +38,12 @@ import com.rustamft.tasksft.domain.util.ROUTE_EDITOR
 fun EditorScreen(
     viewModel: EditorViewModel = hiltViewModel(),
     navigator: DestinationsNavigator,
-    scaffoldState: ScaffoldState, // From DependenciesContainer.
-    taskState: State<Task> = viewModel.taskFlow.collectAsState(initial = Task())
+    scaffoldState: ScaffoldState // From DependenciesContainer.
 ) {
 
     var fabVisible by remember { mutableStateOf(false) }
+    var reminderSelectorVisible by remember { mutableStateOf(viewModel.reminder != 0L) }
     val onValueChange = { fabVisible = true }
-    val task by taskState
-    // TODO: make the text field values change
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -54,30 +56,61 @@ fun EditorScreen(
                     }
                 ) {
                     Icon(
-                        painter = painterResource(id = R.drawable.ic_add), // TODO: change
-                        contentDescription = stringResource(id = R.string.action_add) // TODO: change
+                        painter = painterResource(id = R.drawable.ic_save),
+                        contentDescription = stringResource(id = R.string.action_save)
                     )
                 }
             }
         }
     ) { paddingValues ->
         Column(modifier = Modifier.padding(bottom = paddingValues.calculateBottomPadding())) {
+
+            val modifier = Modifier
+                .padding(DIMEN_SMALL)
+
             TextField(
-                value = task.title,
+                modifier = modifier,
+                value = viewModel.title,
                 onValueChange = {
-                    task.title = it
+                    viewModel.title = it
                     onValueChange()
                 },
                 placeholder = { Text(text = stringResource(id = R.string.title)) }
             )
             TextField(
-                value = task.description,
+                modifier = modifier,
+                value = viewModel.description,
                 onValueChange = {
-                    task.description = it
+                    viewModel.description = it
                     onValueChange()
                 },
                 placeholder = { Text(text = stringResource(id = R.string.description)) }
             )
+            Row(
+                modifier = modifier,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = stringResource(id = R.string.reminder))
+                Spacer(modifier = Modifier.width(DIMEN_SMALL))
+                Switch(
+                    checked = reminderSelectorVisible,
+                    onCheckedChange = {
+                        reminderSelectorVisible = it
+                        onValueChange()
+                    }
+                )
+            }
+            if (reminderSelectorVisible) {
+                Row(modifier = modifier) {
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "Date")
+                    }
+                    Spacer(modifier = Modifier.width(DIMEN_SMALL))
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "Time")
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/rustamft/tasksft/presentation/screen/editor/EditorViewModel.kt
+++ app/src/main/java/com/rustamft/tasksft/presentation/screen/editor/EditorViewModel.kt
@@ -1,12 +1,18 @@
 package com.rustamft.tasksft.presentation.screen.editor
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.rustamft.tasksft.domain.usecase.GetListOfTasksUseCase
 import com.rustamft.tasksft.presentation.screen.navArgs
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.emptyFlow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withTimeout
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,9 +22,23 @@ class EditorViewModel @Inject constructor(
 ) : ViewModel() {
 
     private val navArgs = savedStateHandle.navArgs<EditorScreenNavArgs>()
-    val taskFlow = if (navArgs.indexOfTaskInList == -1) {
+    private val taskFlow = if (navArgs.indexOfTaskInList == -1) {
         emptyFlow()
     } else {
         getListOfTasksUseCase.execute().map { it[navArgs.indexOfTaskInList] }
+    }
+    var title by mutableStateOf("")
+    var description by mutableStateOf("")
+    var reminder by mutableStateOf(0L)
+
+    init {
+        viewModelScope.launch {
+            withTimeout(3000) {
+                taskFlow.collect { task ->
+                    title = task.title
+                    description = task.description
+                }
+            }
+        }
     }
 }
