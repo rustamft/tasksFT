5a14fbaf9c0c865fa7e3974a5b6a41cf7d481cf9
rustamft <rustam.ft@pm.me>
2022-08-07 01:16:59 +0500

    Rewrote Gradle build scripts from Groovy to Kotlin, updated dependencies.

---
+++ app/build.gradle.kts
@@ -0,0 +1,105 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
+    id("com.google.devtools.ksp") version "${Constants.KOTLIN_VERSION}-1.0.6"
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+}
+
+kotlin {
+    sourceSets {
+        debug {
+            kotlin.srcDir("build/generated/ksp/debug/kotlin")
+        }
+        release {
+            kotlin.srcDir("build/generated/ksp/release/kotlin")
+        }
+    }
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        applicationId = "com.rustamft.tasksft"
+        minSdk = 21
+        targetSdk = 32
+        versionCode = 1
+        versionName = "0.9.4"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.2.0"
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+
+    implementation(project(":domain"))
+    implementation(project(":data"))
+    // Compose
+    val composeVersion = "1.3.0-alpha02"
+    implementation("androidx.compose.ui:ui:$composeVersion")
+    implementation("androidx.compose.material:material:$composeVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeVersion")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:$composeVersion")
+    debugImplementation("androidx.compose.ui:ui-tooling:$composeVersion")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:$composeVersion")
+    implementation("androidx.activity:activity-compose:1.5.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
+    // Accompanist
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.24.11-rc")
+    // Compose destinations
+    val composeDestinationsVersion = "1.7.15-beta"
+    implementation("io.github.raamcosta.compose-destinations:core:$composeDestinationsVersion")
+    ksp("io.github.raamcosta.compose-destinations:ksp:$composeDestinationsVersion")
+    // Koin
+    implementation("io.insert-koin:koin-android:${Constants.KOIN_VERSION}")
+    implementation("io.insert-koin:koin-androidx-compose:${Constants.KOIN_VERSION}")
+    testImplementation("io.insert-koin:koin-test:${Constants.KOIN_VERSION}")
+    testImplementation("io.insert-koin:koin-test-junit4:${Constants.KOIN_VERSION}")
+    // WorkManager
+    implementation("androidx.work:work-runtime-ktx:2.7.1")
+    // Coroutine test
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:${Constants.COROUTINE_VERSION}")
+    // MockK
+    testImplementation("io.mockk:mockk:1.12.4")
+    // LeakCanary
+    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.9.1")
+}
---
+++ data/build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+}
+
+android {
+    compileSdk = 32
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 32
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    namespace = "com.rustamft.data"
+}
+
+dependencies {
+
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+
+    implementation(project(":domain"))
+    // DocumentFile
+    implementation("androidx.documentfile:documentfile:1.0.1")
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:${Constants.COROUTINE_VERSION}")
+    // DataStore
+    implementation("androidx.datastore:datastore:1.0.0")
+    // Room
+    val roomVersion = "2.5.0-alpha02"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    kapt("androidx.room:room-compiler:$roomVersion")
+    // Gson converter
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    // Koin
+    implementation("io.insert-koin:koin-android:${Constants.KOIN_VERSION}")
+    testImplementation("io.insert-koin:koin-test:${Constants.KOIN_VERSION}")
+    testImplementation("io.insert-koin:koin-test-junit4:${Constants.KOIN_VERSION}")
+}
